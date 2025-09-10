4ac0cfb4b0a35361dbc7c1bd459a309c9e69f777
Kaykemelo <kaykeoliver11@gmail.com>
2025-09-10 16:46:38 -0300

    feat: creation of icon on the web pages

--- template/championshipHeader.php
+++ template/championshipHeader.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campeonatos - Sports FJU</title>
+    <link rel="icon" href="../../sportsfju/assets/images/universal.png" type="image/png">
     <link rel="stylesheet" href="../../sportsfju/assets/css/championship.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
