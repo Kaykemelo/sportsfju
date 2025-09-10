67bc3d33d90cc18d6679ce09cef33b05a3c9b16b
Kaykemelo <kaykeoliver11@gmail.com>
2025-09-10 18:29:17 -0300

    refactor: path of registration of users

--- view/admin/user/register.php
+++ view/admin/user/register.php
@@ -23,7 +23,7 @@ include '../sportsfju/template/FormHeader.php';
     </div>
 
     <div class="d-flex justify-content-between">
-        <a href="?route=usuarios" class="btn btn-secondary">Voltar</a>
+        <a href="?route=web" class="btn btn-secondary">Voltar</a>
         <button type="submit" class="btn btn-primary">Enviar</button>
 
     </div>
